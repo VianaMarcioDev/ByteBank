7f83e89499deb88cc9a27571f8ee44ee037319fd
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-11-05 22:29:53 -0300

    Create ContaCorrente class

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank;
+
+ContaCorrente contaDoMarcio = new ContaCorrente();
+contaDoMarcio.conta = "1000";
+contaDoMarcio.titular = "Márcio Viana";
+contaDoMarcio.numero_agencia = 15;
+contaDoMarcio.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Márcio : R$ " + contaDoMarcio.saldo);
